cc46dfaff7c85466d9ef51613a256003edea6fec
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2020-10-27 21:27:01 +0800

    add react hooks in TodoForm

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react"; 
+import PropTypes from 'prop-types';
 
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
+    const [value, setValue] = useState("");
+    const handleFormSubmit = e => {
+        e.preventDefault();
+        if (!value) {
+            alert("Silahkan isi inputan anda!");
+            return;
+        }
+        addTodo(value);
+        setValue("");
+    }
+    console.log(value);
     return (
         <section className="add">
-            <div className="add-form">
-                <input type="text" className="add-input"/>
+            <form className="add-form" onSubmit={handleFormSubmit}>
+                <input type="text" className="add-input" value={value} onChange={e => setValue(e.target.value)} />
                 <button className="add-btn main-black-color">Enter</button>
-            </div>
+            </form>
         </section>
     );
+}
+
+TodoForm.propTypes = {
+    addTodo: PropTypes.func.isRequired
 }
 
 export default TodoForm;
